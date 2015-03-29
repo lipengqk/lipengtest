95c251626e9682e773ed3cccc9e5b389920a5ef9
lipengqk <lipeng@lipengdeMacBook-Pro.local>
2015-03-29 15:13:44 +0800

    Initial Commit

---
+++ love/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  love
+//
+//  Created by 李碰 on 15/3/29.
+//  Copyright (c) 2015年 三瑞点成. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
